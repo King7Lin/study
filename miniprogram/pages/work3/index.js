8dbd19bc259a100c02a312125ae65b323558f5cb
Lin040 <1978331141@qq.com>
2021-09-22 12:07:39 +0800

    in

--- miniprogram/pages/work3/index.js
+++ miniprogram/pages/work3/index.js
@@ -7,11 +7,17 @@ Page({
     let n1=Number(e.detail.value.num1)
     let n2=Number(e.detail.value.num2)
     if(n1>n2){
+      this.setData({
       result:'第一个数大'
+    })
     }else if(n2>n1){
+      this.setData({
       result:'第二个大'
+    })
     }else{
+      this.setData({
       result:'一样大'
+    })
     }
   }
 })
