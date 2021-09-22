894066636acd3e3dfa66b416d3f5c84f0a98baa2
Lin040 <1978331141@qq.com>
2021-09-22 12:05:59 +0800

    lin

--- miniprogram/pages/work3/index.js
+++ miniprogram/pages/work3/index.js
@@ -1,66 +1,17 @@
-// miniprogram/pages/work3/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    result:'不知道'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  submit(e){
+    console.log(e)
+    let n1=Number(e.detail.value.num1)
+    let n2=Number(e.detail.value.num2)
+    if(n1>n2){
+      result:'第一个数大'
+    }else if(n2>n1){
+      result:'第二个大'
+    }else{
+      result:'一样大'
+    }
   }
 })
