7f8386d0859db0735d26ed5242ce89e216cce71b
Lin040 <1978331141@qq.com>
2021-10-08 15:20:45 +0800

    lin

--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,5 +1,15 @@
 Page({
   data:{
-    msg:'惊喜不惊喜'
+    msg:'惊喜不惊喜',
+    list:{
+      1:'student1',
+      2:'student2',
+      3:'student3',
+      4:'student4',
+      5:'student5',
+      6:'student6',
+      7:'student7',
+
+    }
   }
 })
