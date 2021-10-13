9ce8ae217253fc33a51d1bf7d6ad2f32f8152107
Lin04 <asdasd#qq.com>
2021-10-13 10:00:25 +0800

    '4p'

---
+++ miniprogram/pages/work4 copy/index.js
@@ -0,0 +1,29 @@
+Page({
+  data:{
+    list:[
+      {name:'C',active:true},
+      {name:'D',active:false},
+      {name:'E',active:false},
+      {name:'F',active:false},
+      {name:'G',active:false},
+      {name:'A',active:false},
+      {name:'B',active:false},
+    ],
+    src:'images/c.png'
+  },
+  click(e){
+    console.log(e)
+    let num=e.currentTarget.dataset.id
+    let l=this.data.list
+    l.forEach(v=>{
+      v.active=false
+    })
+    l[num].active=true
+    let src='images/'+l[num].name.toLowerCase()+'.png'
+    this.setData({
+      list:l,
+      src
+    })
+  }
+   
+})
