0d8ac23161bd2351278c7556d40b0ad3930eb5f3
Lin040 <1978331141@qq.com>
2021-10-27 21:58:25 +0800

    10.27 -22:00

--- miniprogram/pages/work8/map/map.js
+++ miniprogram/pages/work8/map/map.js
@@ -1,66 +1,11 @@
-// pages/work8/map/map.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    data:{
+        markers:[{
+            iconPath:'../images/mark.png',
+            id:0,
+            latitude:23.3882,
+            longitude:113.4490,
+            title:'图文楼',
+        }],
     }
 })
