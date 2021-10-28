4aa1cae7305b6131bb08d8a13073cac13ad96fd1
Lin040 <1978331141@qq.com>
2021-10-28 19:20:30 +0800

    10.28

--- miniprogram/pages/work6/divorce.js
+++ miniprogram/pages/work6/divorce.js
@@ -1,14 +1,12 @@
-var num=0
 Page({
     data:{
         fei:300,
         feiban:350,
-        condition:true
+        condition:false
     },
     
     switchchange(e){
         console.log(e)
-        num=Math.random()+1
         this.setData({
             condition:e.detail.value
         })
@@ -17,37 +15,19 @@ Page({
         console.log(e)
         let bd=Number(e.detail.value.property)
         if(bd<=2000000){
-            if(num!=0){
-                this.setData({
-                    fei:600,
-                    feiban:300
-                })
-            }else{
                 this.setData({
                 fei:300,
                 feiban:150
             })
-            }
-            
         }else{
             let fei=(bd-200000)*0.005+300
-             let feiban=fei/2
-             
-           if(num!=0){
-               fei*=2
-               feiban*=2
-               fei=fei.toFixed(2)
-            feiban=feiban.toFixed(2)
-                this.setData({
-                    fei,feiban
-                })
-           }else{
+             let feiban=fei/2     
             fei=fei.toFixed(2)
             feiban=feiban.toFixed(2)
                 this.setData({
                     fei,feiban
                 })
-           }
+           
             
             
         }
--- miniprogram/pages/work6/property.js
+++ miniprogram/pages/work6/property.js
@@ -1,7 +1,12 @@
+var num=0
 Page({
     data:{
         fei:0,
         feiban:0
+    },
+    switchchange(e){
+        console.log(e)
+       num=Math.random()+1
     },
     formsubmit(e){
         console.log(e)
@@ -19,26 +24,29 @@ Page({
             fei+=2250+(bd-100000)*0.02
         }else if(bd<500000){
             fei=50
-            fei+=4250+(bd-100000)*0.02
+            fei+=4250+(bd-200000)*0.015
         }else if(bd<1000000){
             fei=50
-            fei+=9750+(bd-500000)*0.01
+            fei+=8750+(bd-500000)*0.01
         }else if(bd<2000000){
             fei=50
-            fei+=14750+(bd-1000000)*0.009
+            fei+=13750+(bd-1000000)*0.009
         }else if(bd<5000000){
             fei=50
-            fei+=23750+(bd-2000000)*0.008
+            fei+=22750+(bd-2000000)*0.008
         }else if(bd<10000000){
             fei=50
-            fei+=47750+(bd-5000000)*0.007
+            fei+=46750+(bd-5000000)*0.007
         }else if(bd<20000000){
             fei=50
-            fei+=82750+(bd-10000000)*0.006
+            fei+=81750+(bd-10000000)*0.006
         }else{
             fei=50
-            fei+=142750+(bd-10000000)*0.006
+            fei+=141750+(bd-10000000)*0.006
         }
+            if(num!=0){
+               fei*=2
+            }
             feiban=fei/2
             fei=fei.toFixed(2)
             feiban=feiban.toFixed(2)
