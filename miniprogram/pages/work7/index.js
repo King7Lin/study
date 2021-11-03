98f4ba9331b2eb51d359a55c61bd0dc75f6f95b3
Lin040 <1978331141@qq.com>
2021-11-03 10:52:41 +0800

    lin

--- miniprogram/pages/work7/index.js
+++ miniprogram/pages/work7/index.js
@@ -1,10 +1,12 @@
-const baseurl='https://api.map.baidu.com/weather/v1/?data_type=all&ak=1zBnGp4dKBoGyPgx0sZN6EkPr7fEZXWD&district_id='
+const baseurl='https://free-api.heweather.net/s6/weather/now?key=b98d5bd54c4d417bb169b189b24f4bb8&location='
 Page({
     data:{
         distinct_id:'',
         city:'',
-        temp:'',
-        wind_dir:''
+        tmp:'',
+        wind_dir:'',
+        latiude:0,
+        longitue:0,
     },
     input(e){
         console.log(e)
@@ -21,13 +23,25 @@ Page({
             url,
             success(e){
                 console.log(e)
-                let {temp,wind_dir}=e.data.result.now
-                let city=e.data.result.location.city
+                let {tmp,wind_dir}=e.data.HeWeather6[0].now
+                let city=e.data.HeWeather6[0].basic.location
                 that.setData({
-                    temp,wind_dir,city
+                    tmp,wind_dir,city
                 })
 
             }
+        })
+    },
+    onLoad(){
+        let that =this
+        wx.getLocation({
+          success(e){
+              console.log(e)
+              let {latiude,longitue} =e
+              that.setData({
+                  latiude,longitue
+              })
+          }
         })
     }
 })
