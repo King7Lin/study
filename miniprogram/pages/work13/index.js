1f558e4223a6167ce8f81ea85dd6988115d6d7aa
Lin040 <1978331141@qq.com>
2021-11-26 15:40:28 +0800

    11.26

--- miniprogram/pages/work13/index.js
+++ miniprogram/pages/work13/index.js
@@ -1,24 +1,105 @@
 const db = wx.cloud.database()
-const musiclist = db.collection('musiclist')
+const ml = db.collection('musiclist')
 const audioCtx=wx.createInnerAudioContext()
 Page({
+  data:{
+    tab:0,
+    item:0,
+    playIndex:3,
+    state:'paused',
+    play:{
+      curremtTime:0,
+      duration:0,
+      percent:0,
+      title:'',
+      singer:'',
+      coverImg:''
+    }
+  },
   onLoad(){
-    // wx.setInnerAudioOption({
-    //   mixWithOther: true,
-    //   obeyMuteSwitch:false,
-    // });
-    musiclist.get().
-    then(res=>{
+    ml.get().then(res=>{
       console.log(res)
       this.setData({
         musiclist:res.data
       })
+      this.selectMusic(0)
+    })
+    audioCtx.onError(()=>{
+      console.log('播放失败：'+audioCtx.src)
+    })
+    audioCtx.onEnded(()=>{
+      this.next()
+    })
+    audioCtx.onCanplay(()=>{
+      console.log('onCanplay')
+      this.setData({
+        'play.duration':audioCtx.duration
+      })
+      if(this.data.state=='running'){
+        audioCtx.play()
+      }
+    })
+    audioCtx.onTimeUpdate(()=>{
+      this.setData({
+        'play.duration':audioCtx.duration,
+        'play.currentTime':audioCtx.currentTime,
+        'play.percent':audioCtx.currentTime/audioCtx.duration*100
+      })
     })
   },
-  click(e){
+  changeItem(e){
     console.log(e)
-    let index = e.currentTarget.dataset.id
-    audioCtx.src = this.data.musiclist[index].src
+    this.setData({
+      item:e.currentTarget.dataset.item
+    })
+  },
+  changeTab(e){
+    console.log(e)
+    this.setData({
+      tab:e.detail.current
+    })
+  },
+  selectMusic(index){
+    console.log(index)
+    let music = this.data.musiclist[index]
+    audioCtx.src = music.src
+    this.setData({
+      playIndex:index,
+      play:music,
+      'play.currentTime':0,
+      'play.duration':0,
+      'play.percent':0
+    })
+  },
+  play(){
     audioCtx.play()
+    this.setData({
+      state:'running'
+    })
+  },
+  pause(){
+    audioCtx.pause()
+    this.setData({
+      state:'paused'
+    })
+  },
+  next(){
+    let index = (this.data.playIndex>=this.data.musiclist?0:this.data.playIndex+1)
+    console.log(index)
+    this.selectMusic(index)
+    if(this.data.state=='running'){
+      this.play()
+    }
+  },
+  change(e){
+    console.log(e)
+    this.selectMusic(e.currentTarget.dataset.index)
+    this.play()
+  },
+  changePage(e){
+    console.log(e)
+    this.setData({
+      item:e.currentTarget.dataset.page
+    })
   }
 })
